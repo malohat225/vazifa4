88d49903527a1944f44245cf48d274aeb12eb2b5
malohat <malohatnuriddinova225@gmail.com>
2026-03-22 14:50:50 +0500

    Update school.php

--- school.php
+++ school.php
@@ -2,22 +2,14 @@
 $servername = "127.0.1.15"; 
 $username = "root";
 $password = ""; 
-$dbname = "maktab";   // sening database nomi
+$dbname = "maktab";  
 $port = 3306;
 
-// MySQL ga ulanish
 $conn = mysqli_connect($servername, $username, $password, $dbname, $port);
 
-// Ulanish xatosini tekshirish
-if (!$conn) {
-    die("Ulanishda xatolik: " . mysqli_connect_error());
-}
-
-// Ma’lumot olish so‘rovi (school table)
 $sql = "SELECT * FROM school";
 $result = mysqli_query($conn, $sql);
 
-// Natijani chiqarish (line by line)
 if (mysqli_num_rows($result) > 0) {
     while ($row = mysqli_fetch_assoc($result)) {
         echo $row['schooll_name'] . " - " 
@@ -28,7 +20,4 @@ if (mysqli_num_rows($result) > 0) {
 } else {
     echo "Ma’lumot topilmadi";
 }
-
-// Ulanishni yopish
-mysqli_close($conn);
 ?>
